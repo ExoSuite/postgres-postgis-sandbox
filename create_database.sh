21a7096b035aa9e2dbe9ed52d79a9bb5a674cb6a
Loïc Lopez <loic.lopez@epitech.eu>
2019-03-05 21:08:42 -0500

    fix delete and create

--- create_database.sh
+++ create_database.sh
@@ -2,4 +2,8 @@
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d "$POSTGRES_DB" <<-EOSQL
 	CREATE DATABASE "$1";
+	CREATE EXTENSION IF NOT EXISTS postgis;
+    CREATE EXTENSION IF NOT EXISTS postgis_topology;
+    CREATE EXTENSION IF NOT EXISTS fuzzystrmatch;
+    CREATE EXTENSION IF NOT EXISTS postgis_tiger_geocoder;
 EOSQL
--- delete_database.sh
+++ delete_database.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d "$POSTGRES_DB" <<-EOSQL
-	DROP DATABASE $1;
+	DROP DATABASE "$1";
 EOSQL
