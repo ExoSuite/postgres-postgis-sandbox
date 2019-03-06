e44d69093f3bc612eb07c1f68fceb7506e9bec34
Loïc Lopez <loic.lopez@epitech.eu>
2019-03-05 21:15:55 -0500

    fix create with postgis extension

--- create_database.sh
+++ create_database.sh
@@ -7,3 +7,10 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d "$POSTGRES_DB" <<-EOSQL
     CREATE EXTENSION IF NOT EXISTS fuzzystrmatch;
     CREATE EXTENSION IF NOT EXISTS postgis_tiger_geocoder;
 EOSQL
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d "$1" <<-EOSQL
+	CREATE EXTENSION IF NOT EXISTS postgis;
+    CREATE EXTENSION IF NOT EXISTS postgis_topology;
+    CREATE EXTENSION IF NOT EXISTS fuzzystrmatch;
+    CREATE EXTENSION IF NOT EXISTS postgis_tiger_geocoder;
+EOSQL
